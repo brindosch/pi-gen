ece1932eef5e27f1f3fe03a4c38d3fbc37a1beed
brindosch <edeltraud70@gmx.de>
2017-08-19 19:03:05 +0200

    add travis

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# sf_upload <deploylist> <sf_dir>
+sf_upload()
+{
+	/usr/bin/expect <<-EOD
+	spawn scp $1 hyperionsf37@frs.sourceforge.net:/home/frs/project/hyperion-project/$2
+	expect "*(yes/no)*"
+	send "yes\r"
+	expect "*password:*"
+	send "$SF\r"
+	expect eof
+	EOD
+}
+
+deploylist="HyperBian.zip"
+if [[ $TRAVIS_EVENT_TYPE != 'pull_request' ]]; then
+	cd $TRAVIS_BUILD_DIR/deploy
+	echo "UPLOAD IMAGE: ${deploylist}"
+	sf_upload $deploylist dev/hyperbian
+fi
+
